d6e44103030d2729abcece4932b60c02ba2a9f51
Mihnea Mandache <mihnea.mandache@yahoo.com>
2018-04-08 23:12:08 +0100

    trying to solve heroku problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :update]
+  resources :routes, only: [:create]
   resources :routes, only: [:create] do
     member do
       get 'configure', to: :configure
